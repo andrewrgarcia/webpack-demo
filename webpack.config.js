621c8be793bc127075231cc70b28da63c7cd6996
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-07-10 13:35:10 -0400

    Extracting Boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Caching',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
 };
